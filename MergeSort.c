9457c4af5a4321030da44cf21ac7bcf7260a6d6a
Harsha Sai <131501226+HarshaSai007@users.noreply.github.com>
2024-04-12 10:30:21 +0530

    Fix typo in BinarySearch.c and improve readability in MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -1,78 +1,67 @@
 #include <stdio.h>
 
-void merge(int arr[], int p, int q, int r)
-{
+void merge(int arr[], int low, int mid, int high) {
 
-    int n1 = q - p + 1;
-    int n2 = r - q;
+  int n1 = mid - low + 1;
+  int n2 = high - mid;
 
-    int L[n1], M[n2];
+  int L[n1], M[n2];
 
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[p + i];
-    for (int j = 0; j < n2; j++)
-        M[j] = arr[q + 1 + j];
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[low + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[mid + 1 + j];
 
-    int i, j, k;
-    i = 0;
-    j = 0;
-    k = p;
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = low;
 
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= M[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = M[j];
-            j++;
-        }
-        k++;
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
     }
+    k++;
+  }
 
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
 
-    while (j < n2)
-    {
-        arr[k] = M[j];
-        j++;
-        k++;
-    }
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
 }
 
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r)
-    {
-        int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
+void mergeSort(int arr[], int low, int high) {
+  if (low < high) {
+    int mid = low + (high - low) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+  }
 }
 
-void printArray(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
 }
 
-int main()
-{
-    int arr[] = {6, 5, 12, 10, 9, 1};
-    int size = sizeof(arr) / sizeof(arr[0]);
+int main() {
+  int arr[] = {6, 5, 12, 10, 9, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
 
-    mergeSort(arr, 0, size - 1);
+  mergeSort(arr, 0, size - 1);
 
-    printf("Sorted array: \n");
-    printArray(arr, size);
+  printf("Sorted array: \n");
+  printArray(arr, size);
 }
